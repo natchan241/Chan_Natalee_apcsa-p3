19f323b43e66c3c46c0be33aeace10aeaae6a9dc
Natalee Chan <channatalee@gmail.com>
2019-01-31 23:13:28 -0800

    Unit 00 Assignments

---
+++ Unit00/src/AsciiArt.java
@@ -0,0 +1,29 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name: Natalee Chan
+//Date: January 31, 2019
+//Class: APCSA
+//Lab: Oa 3
+
+public class AsciiArt
+{
+	public static void main ( String[] args )
+	{
+		System.out.println("Your Name: Natalee Chan \n\n" );
+		System.out.println("What type of ANIMAL YOU WILL DRAW?" );
+		System.out.println("\n\n\n\n" );
+
+		System.out.println("         /\\\\           /\\\\       " );
+		System.out.println("         |     --------     |          " );
+		System.out.println("         |   ^          ^   |          " );
+		System.out.println("         |   [------]       |        " );
+		System.out.println("         |      ______      |        " );
+		System.out.println("         --------------------        " );
+
+		System.out.println(" \n\n\n\nHelpFul Hints" );
+		System.out.println("\\\\ draws one backslash on the screen!\n" );
+		System.out.println("\\\" draws one double quote on the screen!\n" );
+		System.out.println("\\\' draws one single quote on the screen!\n" ); 
+	}
+}
---
+++ Unit00/src/AsciiBox.java
@@ -0,0 +1,27 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name: Natalee Chan
+//Date: January 31, 2019
+//Class: APCSA
+//Lab: Oa 1
+
+public class AsciiBox
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Name: Natalee Chan \t  Date: January 31, 2019 \n\n" );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+
+	}
+}
