28fa7457cfdecaf8f4c89f92029006089c039a6c
Natalee Chan <channatalee@gmail.com>
2019-03-12 18:42:18 -0700

    Unit 09 Assignments

---
+++ Unit09/src/ListOddToEven.java
@@ -0,0 +1,34 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name: Natalee Chan
+//Date: March 12, 2019
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListOddToEven
+{
+	public static int go( List<Integer> ray )
+	{
+		
+		for (int i = 0; i < ray.size(); i++)
+		{
+			if (ray.get(i) % 2 == 1)
+			{
+				int result = 0;
+				for (int j = i; j < ray.size(); j++)
+				{
+					if (ray.get(j) % 2 == 0)
+					{
+						result = j;
+					}
+				}
+				if (result > 0)
+				{
+					return result - i;
+				}
+			}
+		}
+		return -1;
+	}
+}
