0711026f261e1a3891acc2098528aef9b5420802
Natalee Chan <channatalee@gmail.com>
2019-04-05 21:23:12 -0700

    Unit 13 Assignments

---
+++ Unit13/src/NumberShifterRunner.java
@@ -0,0 +1,23 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name: Natalee Chan
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import static java.lang.System.*;
+
+public class NumberShifterRunner
+{
+	public static void main( String args[] ) throws IOException
+	{
+		int[] test = NumberShifter.makeLucky7Array(7);
+		System.out.println(Arrays.toString(test));
+		NumberShifter.shiftEm(test);
+		System.out.println(Arrays.toString(test));
+	}
+}
+
+
+
