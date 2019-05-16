34e94c1f5937af0861ff1e7d1cbf0d4ff1cc94ea
Natalee Chan <channatalee@gmail.com>
2019-05-15 22:54:57 -0700

    Unit 17 Assignments

---
+++ Unit17/src/Locatable.java
@@ -0,0 +1,15 @@
+//� A+ Computer Science  -  www.apluscompsci.com
+//Name: Natalee Chan
+//Date: May 15, 2019
+//Class: APCSA
+//Lab: Unit 17 - Star Fighter
+
+public interface Locatable
+{
+    public void setPos( int x, int y);
+    public void setX( int x );
+    public void setY( int y );
+
+    public int getX();
+    public int getY();
+}
